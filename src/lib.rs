f77ac6e63ed06a328ffc109888d719dd8cfc5c63
Nugine <nugine@foxmail.com>
2022-05-15 15:05:04 +0800

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! Safely cast between numbers.
+//!
+//! The extension trait [`NumericCast`] adds a generic method [`numeric_cast`](NumericCast::numeric_cast) for all number types.
+//! The method allows users to safely cast a number to another type without losing precision.
+//! If the value can not be represented by the target type,
+//! the method will panic with a message which tells the value, the source type name and the target type name.
+//!
+//! As [`numeric_cast`](NumericCast::numeric_cast) is marked by `track_caller`, the panic location will be exactly where you call the method.
+//!
+//! # Examples
+//!
+//! ```
+//! use numeric_cast::NumericCast;
+//!
+//! let entries: u64 = 1024;
+//!
+//! let capacity = entries.numeric_cast::<usize>();
+//! let offset: isize = entries.numeric_cast(); // by inference
+//! ```
+//!
+//! ```should_panic
+//! use numeric_cast::NumericCast;
+//!
+//! let n: i32 = -1;
+//! let len: usize = n.numeric_cast(); // panic here
+//! ```
+//!
 #![deny(clippy::all, clippy::missing_inline_in_public_items)]
 // ---
 #![no_std]
